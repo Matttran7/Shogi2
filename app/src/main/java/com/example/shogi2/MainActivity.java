994037ab0e5c0e53f07fda37d0aa57d7b0c8bab1
Matttran7 <matttran385701@gmail.com>
2022-10-06 14:00:14 -0700

    Added basic files

--- app/src/main/java/com/example/shogi2/MainActivity.java
+++ app/src/main/java/com/example/shogi2/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.shogi2;
 
 import android.os.Bundle;
-
+import android.content.pm.ActivityInfo;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
