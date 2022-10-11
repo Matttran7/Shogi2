95c0e4532921a7255a92893f944c87905aae6133
Matttran7 <matttran385701@gmail.com>
2022-10-11 14:07:59 -0700

    Gamestate 1, contributed some to pieces, added graveyard, some to gamestate

--- app/src/main/java/com/example/shogi2/GameState.java
+++ app/src/main/java/com/example/shogi2/GameState.java
@@ -1,6 +1,9 @@
 package com.example.shogi2;
 
-public class GameState {
+import java.util.ArrayList;
+import java.util.Random;
+
+public class GameState extends Button{
     /* [other class]/
     * Board (reference to copy constructor)
     * Pieces
@@ -10,7 +13,7 @@ public class GameState {
      * Grave (2)
      * Button (new game)
      * TextView (____'s Turn / ____ Wins!)
-     * Turn (keep track)
+     * Turn (keep track) X
      *
      * Current selected piece
      * Selected space
@@ -21,22 +24,45 @@ public class GameState {
      * *** if stalemate ***
      * Previous turn state
      * */
+    private boolean Turn; // [true = Player, false = other]
+    private Board board;
+    private Graveyard grave_1;
+    private Graveyard grave_2;
+    private ArrayList<Piece> Pieces1;
+    private ArrayList<Piece> Pieces2;
 
     public GameState() { //Cntr
-
-
+        Turn = first();
+        board = new Board();
+        grave_1 = new Graveyard();
+        grave_2 = new Graveyard();
+        Pieces1 = new ArrayList<Piece>();
+        Pieces2 = new ArrayList<Piece>();
     }
 
     public GameState(GameState orig) { //DEEP COPY cntr
-
-
     }
 
     public String toString(GameState state) {
-
         return null;
     }
 
     //Make methods for defined actions
+    //make pieces
+    private void assignPieces(ArrayList<Piece>){
+        for(piece : Piece.GAME_PIECES){
+
+        }
+    }
+    // see who goes first
+    public boolean first(){
+        Random rand = new Random();
+        int i = rand.nextInt(11);
+        if(i < 6){return true;}
+        else{return false;}
+    }
+    // check and checkmate methods
+    public boolean isChecked(){return false;}
+    public boolean isCheckmated(){return false;}
 
 }
